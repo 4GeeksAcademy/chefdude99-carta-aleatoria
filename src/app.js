78c8a26f7ba898f213c5ab36de3309f326c0ec7d
chefdude99 <chefdude.1999@gmail.com>
2024-10-04 19:09:23 +0000

    Carta aleatoria

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   generarCarta();
+  setInterval(generarCarta, 5000);
 };
 
 function generarCarta() {
@@ -16,4 +17,14 @@ function generarCarta() {
   let numero = [2, 3, 4, 5, 6, 7, 8, 9, "A", "J", "Q", "K"];
   let numeroRandom = numero[Math.floor(Math.random() * numero.length)];
   console.log(paloRandom, numeroRandom);
+  document.getElementById("palo").innerHTML = paloRandom;
+  document.getElementById("numero").innerHTML = numeroRandom;
+  document.getElementById("palo-abajo").innerHTML = paloRandom;
+  if (paloRandom == "♥" || paloRandom == "♦") {
+    document.getElementById("palo").style.color = "red";
+    document.getElementById("palo-abajo").style.color = "red";
+  } else {
+    document.getElementById("palo").style.color = "black";
+    document.getElementById("palo-abajo").style.color = "black";
+  }
 }
